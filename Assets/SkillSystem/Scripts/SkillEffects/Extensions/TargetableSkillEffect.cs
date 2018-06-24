d425a52064f09548f12378e0a85ed18c575ff5a6
ButterCroissant\steviegiovanni <steviegiovanni@gmail.com>
2018-06-25 06:45:13 +0800

    removed itargetableeffect interface

--- Assets/SkillSystem/Scripts/SkillEffects/Extensions/TargetableSkillEffect.cs
+++ Assets/SkillSystem/Scripts/SkillEffects/Extensions/TargetableSkillEffect.cs
@@ -6,21 +6,7 @@ namespace GameSystems.SkillSystem{
 	/// <summary>
 	/// Targetable skill effect
 	/// </summary>
-	public class TargetableSkillEffect : SkillEffect, ITargetableEffect {
-		/// <summary>
-		/// the target of the effect
-		/// </summary>
-		private GameObject _target;
-
-		#region ITargetableEffect implementation
-
-		public GameObject Target {
-			get {return _target;}
-			set {_target = value;}
-		}
-
-		#endregion
-
+	public class TargetableSkillEffect : SkillEffect{
 		public TargetableSkillEffect():base(){}
 		public TargetableSkillEffect(float delay):base(delay){}
 		public TargetableSkillEffect(TargetableSkillEffectAsset asset):base(asset){}
